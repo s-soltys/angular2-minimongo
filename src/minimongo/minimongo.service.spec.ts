fb7522f9353d5ff1d5502e6cdb406f6891b4ae8a
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2017-02-19 14:49:39 +0100

    Test manipulating collections.

--- src/minimongo/minimongo.service.spec.ts
+++ src/minimongo/minimongo.service.spec.ts
@@ -65,30 +65,47 @@ describe('MinimongoService', () => {
   });
 
   it('upsert, find and remove a multiple entities', done => {
-    let eOriginal: TestEntity = { name: 'xxx', value: 123, tags: ['a', 2, 'c'] };
-    let eSaved: TestEntity;
-    let eRetrieved1: TestEntity;
-    let eRetrieved2: TestEntity;
+    let eOriginal: TestEntity[] = [
+      { name: 'A', value: 1 },
+      { name: 'A', value: 2 },
+      { name: 'B', value: 3 }
+    ];
+    let eSaved: TestEntity[];
+    let eRetrievedAll: TestEntity[];
+    let eRetrievedA: TestEntity[];
+    let eRetrievedB: TestEntity[];
     let deleteResult: any;
-    let eRetrievedAfterDelete: TestEntity;
+    let eRetrievedAfterDelete: TestEntity[];
+    
+    let removeAll = _collection.find({}).flatMap(o => o.map(x => _collection.remove(x._id))).concatAll();
 
     _collection
       .upsert(eOriginal)
         .do(o => eSaved = o)
-      .switchMap(o => _collection.findOne({ _id: o._id }))
-        .do(o => eRetrieved1 = o)
-      .switchMap(o => _collection.findOne(o._id))
-        .do(o => eRetrieved2 = o)
-      .switchMap(o => _collection.remove(o._id))
+      .switchMap(o => _collection.find({}))
+        .do(o => eRetrievedAll = o)
+      .switchMap(o => _collection.find({ name: 'A' }))
+        .do(o => eRetrievedA = o)
+      .switchMap(o => _collection.find({ name: 'B' }))
+        .do(o => eRetrievedB = o)
+      .switchMap(o => removeAll)
         .do(o => deleteResult = o)
-      .switchMap(o => _collection.findOne({ _id: eSaved._id }))
+      .switchMap(o => _collection.find({}))
         .do(o => eRetrievedAfterDelete = o)
       .subscribe(_ => {
         expect(eSaved).toEqual(eOriginal);
-        expect(eRetrieved1).toEqual(eOriginal);
-        expect(eRetrieved2).toEqual(eOriginal);
+        expect(eRetrievedAll).toEqual(eOriginal);
+
+        expect(eRetrievedA.length).toBe(2);
+        expect(eRetrievedB.length).toBe(1);
+        expect(eRetrievedA).toEqual(eOriginal.filter(o => o.name === 'A'));
+        expect(eRetrievedB).toEqual(eOriginal.filter(o => o.name === 'B'));
+
         expect(deleteResult).toBeUndefined();
-        expect(eRetrievedAfterDelete).toBeNull();
+
+        expect(eRetrievedAfterDelete.length).toBe(0);
+        expect(eRetrievedAfterDelete).toEqual([]);
+        
         done();
       });
   });
